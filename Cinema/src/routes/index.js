9311c6c375158126a9767da67fc91444306d42d3
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-12-11 13:10:06 +0000

    base for navigation

---
+++ Cinema/src/routes/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+import Main from "../views/Main";
+
+// Sets up all the routs in the app
+const Routes = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Main">
+				<Stack.Screen name="Main" component={Main} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default Routes;
