8867274a0aa92fd285d187b0add958af4af936d2
Rabbabari <rebekkal22@ru.is>
2023-12-12 14:44:08 +0000

    Movie details

--- Cinema/src/views/CinemaDetails/index.js
+++ Cinema/src/views/CinemaDetails/index.js
@@ -1,18 +1,19 @@
 import { Text, View, TouchableOpacity } from "react-native";
-import { useGetMoviesQuery } from "../../services/cinemaApi";
+import { useGetMoviesByCinemaQuery } from "../../services/cinemaApi";
 import CinemaInfo from "../../components/CinemaInfo";
 
 const CinemaDetails = ({ route }) => {
 	const { cinema } = route.params;
-	// const { data, isLoading, error } = useGetMoviesQuery();
-	// if (isLoading) {
-	// 	return <Text>Loading data...</Text>;
-	// }
+	const { data, isLoading, error } = useGetMoviesByCinemaQuery(cinema.name);
+	console.log(data);
+	if (isLoading) {
+		return <Text>Loading data...</Text>;
+	}
 
-	// if (error) {
-	// 	console.error(error); // Logs complete error object to console
-	// 	return <Text>Error occurred: {JSON.stringify(error, null, 2)}</Text>;
-	// }
+	if (error) {
+		console.error(error); // Logs complete error object to console
+		return <Text>Error occurred: {JSON.stringify(error, null, 2)}</Text>;
+	}
 
 	// console.log(data);
 	return (
--- Cinema/src/views/MovieDetails/index.js
+++ Cinema/src/views/MovieDetails/index.js
@@ -1,7 +1,40 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, FlatList } from "react-native";
+import MovieThumbnail from "../../components/MovieThumbnail";
 
-const MovieDetails = ({}) => {
-	return <Text>Movie Detail</Text>;
+const MovieDetails = (movie) => {
+	return (
+		<View>
+			<Text>Movie Detail</Text>
+			<FlatList
+				data={movie}
+				renderItem={({
+					item: {
+						title,
+						poster,
+						plot,
+						durationMinutes,
+						year,
+						genres,
+						showtimes,
+					},
+				}) => {
+					return (
+						<MovieThumbnail
+							title={title}
+							poster={poster}
+							plot={plot}
+							durationMinutes={durationMinutes}
+							year={year}
+							genres={genres}
+							showtimes={showtimes}
+						/>
+					);
+				}}
+				contentContainerStyle={{ flexGrow: 1 }}
+				style={{ flex: 1 }}
+			></FlatList>
+		</View>
+	);
 };
 
 export default MovieDetails;
