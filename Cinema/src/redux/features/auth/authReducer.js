0651e893cc2de9508ddbd967e16823c6cdbf4a28
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-12-14 12:31:19 +0000

    added token to axios requests

--- Cinema/src/redux/features/auth/authReducer.js
+++ Cinema/src/redux/features/auth/authReducer.js
@@ -1,21 +1,21 @@
 // reducers/authReducer.js
-import { FETCH_TOKEN_SUCCESS, SET_ACCESS_TOKEN } from './authActions';
+import { FETCH_TOKEN_SUCCESS, SET_ACCESS_TOKEN } from "./authActions";
 
 const initialState = {
-  accessToken: null,
+	accessToken: null,
 };
 
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_TOKEN_SUCCESS:
-    case SET_ACCESS_TOKEN:
-      return {
-        ...state,
-        accessToken: action.payload,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case FETCH_TOKEN_SUCCESS:
+		case SET_ACCESS_TOKEN:
+			return {
+				...state,
+				accessToken: action.payload,
+			};
+		default:
+			return state;
+	}
 };
 
 export default authReducer;
